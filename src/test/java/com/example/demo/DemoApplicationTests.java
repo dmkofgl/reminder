6f4384c5ac6e9ebd38b46dbc920fac70741aa967
Uladzislau.Barkou <dmkofgl@gmail.com>
2020-09-02 00:17:04 +0300

    Added Procfile

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.telegram.telegrambots.ApiContextInitializer;
 
 import java.time.Period;
 import java.time.temporal.ChronoUnit;
@@ -9,8 +10,13 @@ import java.time.temporal.ChronoUnit;
 @SpringBootTest
 class DemoApplicationTests {
 
+	static{
+		ApiContextInitializer.init();
+
+	}
 	@Test
 	void contextLoads() {
+
 		String s = "P1Y2m";
 		Period p =Period.parse(s);
 		System.out.println(p);
